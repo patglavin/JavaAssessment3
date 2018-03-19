955dae004b3ac987db8907148115d1d0128dbb00
Patrick Glavin <patjglavin@gmail.com>
2018-03-19 10:14:17 -0400

    add gson dependency

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,10 +1,19 @@
 package parsing_json;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
 
-public class ElementCollection extends ArrayList{
+public class ElementCollection extends ArrayList<Element>{
 
     public Element findByAtomicNumber(int atomic_number) {
+        ListIterator<Element> it = listIterator();
+        System.out.println(listIterator().next());
+        while (listIterator().hasNext()) {
+            System.out.println(it.next());
+            if (it.next().getNumber() == atomic_number) return it.next();
+        }
         return null;
     }
 
