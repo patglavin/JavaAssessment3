871d7113edf89db1b56d33ca35cc50f352d02b5a
Patrick Glavin <patjglavin@gmail.com>
2018-03-19 10:26:51 -0400

    attempt gson integration

--- src/main/java/parsing_json/ElementCollectionInitializer.java
+++ src/main/java/parsing_json/ElementCollectionInitializer.java
@@ -1,7 +1,25 @@
 package parsing_json;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ElementCollectionInitializer {
-    public static ElementCollection generate() {
-        return null;
+
+    private static File file = new File("/Users/patrickglavin/Dev/fourth_hex/fourthAssessment/src/main/resources/periodic_table.json");
+
+    public static ElementCollection generate() throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        ElementCollection elementCollection = new ElementCollection();
+        Gson gson = new GsonBuilder().create();
+        Element[] elements = gson.fromJson(br, Element[].class);
+        for (Element element:elements) {
+            elementCollection.add(element);
+        }
+        return elementCollection;
     }
 }
